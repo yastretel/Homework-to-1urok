98d7e07e9be632f8c19a303c98db36b941eb0640
yastretel <yas.tretel@gmail.com>
2022-07-23 00:55:18 +0300

    task3 complete

---
+++ task 3.py
@@ -0,0 +1,16 @@
+data1 = input("Введите дату:")
+task1 = input("Введите задачу:")
+
+data2 = input("Введите дату:")
+task2 = input("Введите задачу:")
+
+data3 = input("Введите дату:")
+task3 = input("Введите задачу:")
+
+dictionary = {
+    data1  : task1,
+    data2 : task2,
+    data3 : task3
+}
+
+
