2a83115981e652285925afd31af1a3ddf05d8705
yastretel <yas.tretel@gmail.com>
2022-07-23 00:47:15 +0300

    Complete task2

---
+++ task 2.py
@@ -0,0 +1,12 @@
+data1 = input("Введите дату:")
+task1 = input("Введите задачу:")
+
+data2 = input("Введите дату:")
+task2 = input("Введите задачу:")
+
+data3 = input("Введите дату:")
+task3 = input("Введите задачу:")
+
+print(data1, task1)
+print(data2, task2)
+print(data3,task3)
